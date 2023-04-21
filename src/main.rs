52b4c29359a1f6cb3cc74ca1a2495b4fc6dc61aa
bbastanza <bbastanza@gmail.com>
2023-04-21 16:57:04 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use reqwest::Error;
+
+async fn fetch_lorem() -> Result<Vec<String>, Error> {
+    let url = "https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=0&format=text";
+    let response = reqwest::get(url).await?.text().await?;
+
+    let words: Vec<&str> = response.split_ascii_whitespace().collect();
+
+    let x = words
+        .iter()
+        .map(|word| word.chars().filter(|c| c.is_alphabetic()).collect())
+        .collect();
+
+    Ok(x)
+}
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let mut output_count: i32 = 50;
+
+    if args.len() == 2 {
+        output_count = args[1].parse().unwrap();
+    }
+
+    let mut word_count: i32 = 0;
+
+    while output_count > word_count {
+        let lorem = fetch_lorem().await.unwrap();
+
+        for word in lorem {
+            word_count = word_count + 1;
+
+            if word_count > output_count {
+                break;
+            }
+
+            if word == "" {
+                continue;
+            }
+
+            print!("{} ", word);
+        }
+    }
+}
