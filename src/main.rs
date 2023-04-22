05002be20b737631b14c807261fb3b31a42a19df
bbastanza <bbastanza@gmail.com>
2023-04-21 20:38:54 -0400

    print all at once

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use reqwest::Error;
 
 async fn fetch_lorem() -> Result<Vec<String>, Error> {
-    let url = "https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=0&format=text";
+    let url =
+        "https://baconipsum.com/api/?type=meat-and-filler&paras=20&start-with-lorem=0&format=text";
+
     let response = reqwest::get(url).await?.text().await?;
 
-    let words: Vec<&str> = response.split_ascii_whitespace().collect();
-
-    let x = words
-        .iter()
-        .map(|word| word.chars().filter(|c| c.is_alphabetic()).collect())
+    let words: Vec<String> = response
+        .split_ascii_whitespace()
+        .map(|w| w.to_string() + " ")
         .collect();
 
-    Ok(x)
+    Ok(words)
 }
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    let mut output_count: i32 = 30;
+    let mut output_count = 30;
 
     if args.len() == 2 {
         let result = args[1].parse();
@@ -28,23 +28,22 @@ async fn main() {
         }
     }
 
-    let mut word_count: i32 = 0;
+    let mut word_count = 0;
+    let mut output = String::from("");
 
     while output_count > word_count {
         let lorem = fetch_lorem().await.unwrap();
 
         for word in lorem {
-            word_count = word_count + 1;
-
-            if word_count > output_count {
+            if word_count == output_count {
                 break;
             }
 
-            if word == "" {
-                continue;
-            }
+            word_count = word_count + 1;
 
-            print!("{} ", word);
+            output = output + &word;
         }
     }
+
+    println!("{}", output.trim())
 }
